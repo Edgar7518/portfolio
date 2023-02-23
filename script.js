ebcaa429f22925b7d562ae5902133b160c0b2dae
edgar quenum <quenum.edgar@gmail.com>
2023-02-22 19:53:13 -0500

    impléemtation dark mode et description personnelle

--- script.js
+++ script.js
@@ -5,11 +5,11 @@ function changeDarkMode() {
     darkMode = false;
     document.documentElement.style.setProperty("--text-color", "black");
     document.documentElement.style.setProperty("--background-color", "#efe7e5");
-    document.getElementById("dark-light-mode").innerHTML = "Dark mode";
+    document.getElementById("dark-light-mode").innerHTML = "Mode sombre";
   } else {
     darkMode = true;
     document.documentElement.style.setProperty("--text-color", "white");
     document.documentElement.style.setProperty("--background-color", "black");
-    document.getElementById("dark-light-mode").innerHTML = "Light mode";
+    document.getElementById("dark-light-mode").innerHTML = "Mode clair";
   }
 }
