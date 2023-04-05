c1887768ce177c0f8334e80d952eaf828bcfb297
edgar quenum <quenum.edgar@gmail.com>
2023-04-05 13:22:35 -0400

    changement background color en gris clair

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function changeDarkMode() {
   if (darkMode) {
     darkMode = false;
     document.documentElement.style.setProperty("--text-color", "black");
-    document.documentElement.style.setProperty("--background-color", "#e5e7eb");
+    document.documentElement.style.setProperty("--background-color", "#f2f2f2");
     document.getElementById("dark-light-mode").innerHTML = "Mode sombre";
   } else {
     darkMode = true;
